257fc142a94269d49b8f8b926c04abb357562466
Stephen Dios <stephendiosdeveloper@gmail.com>
2022-03-08 13:13:33 -0500

    Finished exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
 const repeatString = function() {
+    let str = arguments[0];
+    let num = arguments[1];
+    let result = "";
 
+    if(num < 0) {
+        result = "ERROR";
+    }
+
+    for(let i = 0; i < num; i++) {
+        result += str;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
 const reverseString = function() {
+    let strArr = [...arguments[0]];
+    let resArr = [];
 
+    for(let i = strArr.length - 1; i >= 0; i--) {
+        resArr.push(strArr[i]);
+    }
+
+    let result = resArr.join("");
+    return result;
 };
 
 // Do not edit below this line
